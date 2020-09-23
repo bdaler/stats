28bef74cd469befb467bf37e09c2e18370e86d5c
bdaler <dalerkbtut@gmail.com>
2020-09-23 21:20:57 +0400

    init.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bdaler/stats
 
 go 1.15
+
+require github.com/bdaler/bank v0.0.0-20200918170546-eb60a567ff17 // indirect
---
+++ pkg/stats/stats.go
@@ -0,0 +1,28 @@
+package stats
+
+import (
+	"github.com/bdaler/bank/pkg/bank/types"
+)
+
+func Avg(payments []types.Payment) types.Money {
+	count := len(payments)
+	sum := 0
+
+	for i := 0; i < count; i++ {
+		sum += int(payments[i].Amount)
+	}
+	avg := sum / count
+
+	return types.Money(avg)
+}
+
+func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
+	sum := types.Money(0)
+
+	for _, payment := range payments {
+		if payment.Category == category {
+			sum += payment.Amount
+		}
+	}
+	return sum
+}
