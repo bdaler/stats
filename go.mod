99f37d59d2b2089b6df3083285e4e7b27868f00c
dbahritdinov <dbahritdinov@htc-cs.ru>
2020-09-24 09:27:41 +0400

    add some tests

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/bdaler/stats
 
 go 1.15
 
-require github.com/bdaler/bank v0.0.0-20200918170546-eb60a567ff17 // indirect
+require github.com/bdaler/bank v0.0.0-20200918170546-eb60a567ff17
--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -1,1 +1,17 @@
 package stats
+
+import (
+	"fmt"
+	"github.com/bdaler/bank/pkg/bank/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{1, 10, "T"},
+		{1, 11, "U"},
+		{1, 12, "R"},
+	}
+	fmt.Println(Avg(payments))
+	//Output:
+	// 11
+}
