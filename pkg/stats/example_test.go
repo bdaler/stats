c847d9ce446216339aeac1e2119528b271e12a89
dbahritdinov <dbahritdinov@htc-cs.ru>
2020-09-24 11:25:29 +0400

    add some tests

--- pkg/stats/example_test.go
+++ pkg/stats/example_test.go
@@ -15,3 +15,14 @@ func ExampleAvg() {
 	//Output:
 	// 7
 }
+
+func ExampleTotalInCategory() {
+	payments := []types.Payment{
+		{1, 10, "T", "OK"},
+		{1, 11, "U", "FAIL"},
+		{1, 12, "R", "INPROGRESS"},
+	}
+	fmt.Println(TotalInCategory(payments, "U"))
+	//Output:
+	// 0
+}
